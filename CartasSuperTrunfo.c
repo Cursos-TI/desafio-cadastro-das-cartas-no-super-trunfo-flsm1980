e2de543bb33c0d68096208cb3af7de2c510d0140
Flávio Maia <flsm1980@yahoo.com>
2025-07-26 00:36:06 +0000

    Nível Mestre - Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,6 @@
 
 // Desafio Super Trunfo - Cidades
 // Nível Mestre
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
 // Teste Flávio Maia
 
 int main() {
